f3890c1d26b1a262b48b41b4efdd4602ac8d94e8
Guillian Prince <guillianprince@R2Hs-MacBook-Pro.local>
2022-01-10 18:36:45 -0500

    easy and very easy challenges

--- easy.js
+++ easy.js
@@ -1,1 +1,5 @@
 //EASY: Create two variables and assign a person’s name to each. Console log a statement that says which name is shorter or longer, and by how many characters.  Example output: “The name Thomas is longer than John by 2 characters”
+var name1 = "Brandon"
+var name2 = "Guillian"
+
+console.log(`${name2} is longer than ${name1} by two letters`);
--- veryeasy.js
+++ veryeasy.js
@@ -1,5 +1,5 @@
 //VERY EASY: Create two variables and assign a number to each. Console log the difference between the numbers. Example output: “The difference between 6 and 2 is 4”
-var num1 = 14
-var num2 = 7
+var num1 = "14"
+var num2 = "7"
 
 console.log(`The difference between ${num1} and ${num2} is 7`)
